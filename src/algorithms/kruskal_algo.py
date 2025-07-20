ca20daf06bd307e1e9cfa1a0f4fe2281ce1feb18
Gabriel Leal <marqsleal@gmail.com>
2025-07-20 10:53:56 -0300

    Feature - Kruskal_algo: find_node_parent e union_nodes

--- src/algorithms/kruskal_algo.py
+++ src/algorithms/kruskal_algo.py
@@ -15,32 +15,63 @@ def merge_sort_edges_by_weight(edges: list) -> list:
 
     middle_node = len(edges) // 2
 
-    left_half = merge_sort_edges_by_weight(edges[:middle_node])
-    right_half = merge_sort_edges_by_weight(edges[middle_node:])
+    edges_left_half = merge_sort_edges_by_weight(edges[:middle_node])
+    edges_right_half = merge_sort_edges_by_weight(edges[middle_node:])
 
-    return merge_lists(left_half, right_half)
+    return merge_lists(edges_left_half, edges_right_half)
 
 
-def merge_lists(left_half: list, right_half: list) -> list:
+def merge_lists(edges_left_half: list, edges_right_half: list) -> list:
     sorted_list = []
     i, j = 0, 0
 
-    while i < len(left_half) and j < len(right_half):
-        if left_half[i][EdgesIndex.WEIGHT.value] < right_half[j][EdgesIndex.WEIGHT.value]:
-            sorted_list.append(left_half[i])
+    while i < len(edges_left_half) and j < len(edges_right_half):
+        if edges_left_half[i][EdgesIndex.WEIGHT.value] < edges_right_half[j][EdgesIndex.WEIGHT.value]:
+            sorted_list.append(edges_left_half[i])
             i += 1
         else:
-            sorted_list.append(right_half[j])
+            sorted_list.append(edges_right_half[j])
             j += 1
     
-    sorted_list.extend(left_half[i:])
-    sorted_list.extend(right_half[j:])
+    sorted_list.extend(edges_left_half[i:])
+    sorted_list.extend(edges_right_half[j:])
 
     return sorted_list
 
 
+def find_node_parent(parent, node):
+    if parent[node] != node:
+        parent[node] = find_node_parent(parent, parent[node])
+    return parent[node]
+
+
+def union_nodes(parent, rank, node1, node2):
+    root1 = find_node_parent(parent, node1)
+    root2 = find_node_parent(parent, node2)
+
+    if root1 != root2:
+        if rank[root1] < rank[root2]:
+            parent[root1] = root2
+
+        elif rank[root1] > rank[root2]:
+            parent[root2] = root1
+
+        else:
+            parent[root2] = root1
+            rank[root1] += 1
+
+
 def kruskal_algorithm(graph: dict):
-    pass
+    edges = []
+    seen = set()
+
+    for from_node in graph:
+        for to_node, weight in graph[from_node]:
+            if (to_node, from_node) not in seen:
+                edges.append((weight, from_node, to_node))
+                seen.add((from_node, to_node))
+    
+    sorted_edges = merge_sort_edges_by_weight(edges)
 
 
 def main():
